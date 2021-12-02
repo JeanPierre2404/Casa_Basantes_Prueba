b6974baaf536537b84a59c66754d432040cc8185
Casa JeanPierre <jean05343@gmail.com>
2021-12-02 18:48:22 -0500

    Casa_Basantes

--- registrar.php
+++ registrar.php
@@ -21,3 +21,18 @@ $consulta="INSERT INTO registro(fecha,taller,kilometraje,costo,tipo,observacione
     mysqli_close($conexion);
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>REGISTRO EXITROSO
+</h3>
+<button href="instalacion.html">REGRESAR AL INICIO
+</button>
+</body>
+</html>
